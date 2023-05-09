0f6907f6ebd4e053ce1d791e29df15021315b52f
xky <cargox@qq.com>
2023-05-09 09:17:34 +0800

    fix: update relative path in html

--- drawing-panel-vite/vite.config.ts
+++ drawing-panel-vite/vite.config.ts
@@ -7,7 +7,9 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root:"./",
+  // root:"./",
+  // publicDir:"./",
+  base:'./',
   server:{
     host:"200.200.170.111"
   },
