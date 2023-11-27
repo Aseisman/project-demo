d9f98f09a717df104e1ac3fe824f08a56b4ec43f
xky <cargox@qq.com>
2023-11-27 17:28:03 +0800

    feat: socks5

--- tresseract/src/App.vue
+++ tresseract/src/App.vue
@@ -1,13 +1,9 @@
 <template>
-  <el-upload
-    ref="uploadRef"
-    v-model:file-list="fileList"
-    action="http://localhost:3000/upload"
-    list-type="picture-card"
-    :on-preview="handlePictureCardPreview"
-    :before-upload="handleUpload"
-  >
-    <el-icon><Plus /></el-icon>
+  <el-upload ref="uploadRef" v-model:file-list="fileList" action="http://localhost:3000/upload" list-type="picture-card"
+    :on-preview="handlePictureCardPreview" :before-upload="handleUpload">
+    <el-icon>
+      <Plus />
+    </el-icon>
   </el-upload>
   <!-- <el-image-viewer
     :hide-on-click-modal="true"
@@ -108,11 +104,9 @@ function drawRectangle(
 ) {
   if (x0 == x1 || y0 == y1) return;
   //dashed
-  return `<div style='position:absolute; top:${y0}px; left:${x0}px; width:${
-    x1 - x0
-  }px; height:${y1 - y0}px; line-height:${
-    y1 - y0
-  }px; font-size:${font_size}px; overflow:hidden;white-space: nowrap;'>${text}</div>`;
+  return `<div style='position:absolute; top:${y0}px; left:${x0}px; width:${x1 - x0
+    }px; height:${y1 - y0}px; line-height:${y1 - y0
+    }px; font-size:${font_size}px; overflow:hidden;white-space: nowrap;'>${text}</div>`;
 }
 const handlePictureCardPreview: UploadProps["onPreview"] = async (
   uploadFile
@@ -158,6 +152,60 @@ const handleUpload: UploadProps["beforeUpload"] = async (uploadFile) => {
     });
 };
 
+/**
+ * 计算字体大小的函数
+ *
+ * @param text 需要计算的文本内容
+ * @param targetWidth 目标宽度
+ * @param targetHeight 目标高度
+ * @param minFontSize 最小字体大小，默认为 10
+ * @param maxFontSize 最大字体大小，默认为 100
+ * @param threshold 阈值，用于控制计算精度，默认为 1
+ */
+const calculateFontSize = (
+  text: string,
+  targetWidth: number,
+  targetHeight: number,
+  minFontSize = 10,
+  maxFontSize = 100,
+  threshold = 1
+): number => {
+  // 创建一个 div 元素作为容器，用于计算实际字体大小
+  const container = document.createElement('div');
+  container.style.display = 'inline-block';
+  container.style.width = `${targetWidth}px`;
+  container.style.height = `${targetHeight}px`;
+  // 创建一个临时的 span 元素，并添加到容器内，用于显示需要计算的文本内容
+  const tempElement = document.createElement('span');
+  tempElement.style.display = 'inline-block';
+  tempElement.style.whiteSpace = 'pre-wrap';
+  tempElement.style.fontFamily = 'Arial, sans-serif';
+  tempElement.innerText = text;
+  container.appendChild(tempElement);
+  document.body.appendChild(container);
+  // 初始化字体大小的取值范围
+  let fontSize = maxFontSize;
+  let lowerBound = minFontSize;
+  let upperBound = maxFontSize;
+  // 使用二分法计算最适合的字体大小
+  while (lowerBound <= upperBound) {
+    fontSize = Math.floor((lowerBound + upperBound) / 2);
+    tempElement.style.fontSize = `${fontSize}px`;
+    // 当计算出的实际宽度或高度大于指定的目标宽度和高度时，缩小字体大小并继续计算
+    if (tempElement.offsetWidth > targetWidth + threshold || tempElement.offsetHeight > targetHeight + threshold) {
+      upperBound = fontSize - 1;
+    } else {
+      // 当计算出的实际宽度和高度小于等于指定的目标宽度和高度时，增大字体大小并继续计算
+      lowerBound = fontSize + 1;
+    }
+  }
+  // 从文档中移除容器及其子元素，并返回计算出的最适合的字体大小
+  document.body.removeChild(container);
+  return fontSize;
+}
+// calculateFontSize("s", 10, 10)
+
+
 // const submitUpload = async () => {
 //   upload.value!.submit();
 //   const worker = await createWorker({
@@ -188,6 +236,7 @@ const handleUpload: UploadProps["beforeUpload"] = async (uploadFile) => {
   /* height: 1000px; */
   /* object-fit: contain; */
 }
+
 .img-text {
   position: relative;
 }
